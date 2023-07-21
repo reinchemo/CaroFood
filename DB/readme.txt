9e7b2ff10ca0f908c3c6423676642e935cffb3ea
Reinhard Chemonges <chemorein24@gmail.com>
2023-07-21 10:39:47 +0300

    first

---
+++ DB/readme.txt
@@ -0,0 +1,30 @@
+Make sure Xampp and Apache is Running
+
+Headover to
+localhost/dashboard/
+
+your  database name is cooking
+
+open another tab and headover to
+localhost/food/
+
+If you dont have an account you can sign up and create a new one then you log in with your details
+
+for you to headover to the admin you use
+
+localhost/food/admin/index.php
+
+username admin
+password admin
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ videos.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Videos - Food &amp; Recipes Web Template</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="header">
+		<div>
+			<a href="index.html"><img src="images/logo.png" alt="Logo"></a>
+		</div>
+		
+	</div>
+	<div class="body">
+		<div>
+			<div class="header">
+				<ul>
+						<li class="current">
+						<a href="index.php">Home</a>
+					</li>
+					<li>
+						<a href="recipes.php">Recipes</a>
+					</li>
+					<li>
+						<a href="featured.php">Featured Recipes</a>
+					</li>
+					
+					<li>
+						<a href="about.php">About</a>
+					</li>
+				</ul>
+			</div>
+			<div class="body">
+				<div id="content">
+					<div>
+						<div>
+							<ul>
+								<li>
+									<h3>The Best Lasagna Ever cooked in Nature 🔥ASMR Cooking</h3>
+									<a href="https://www.youtube.com/embed/67q2MR3C8Xg">
+										<img src="images/video1.jpg" alt="Image"></a>
+									
+								</li>
+								<li>
+									<h3>Divine shredded Chili Chicken baked in a beautiful Shipped Bread.</h3>
+
+								
+
+									<a href="https://www.youtube.com/embed/DGqU6pOJmXk"><img src="images/video2.jpg" alt="Image"></a>
+									<p>
+										
+									</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div>
+				<h3>Cooking Video</h3>
+				<a href="https://www.youtube.com/embed/MNJgeKyt7wM"><img src="images/cooking-video.png" alt="Image"></a>
+
+				
+
+				<span>Vegetable &amp; Rice Topping</span>
+			</div>
+			<div>
+				<h3>Featured Recipes</h3>
+				<ul id="featured">
+					<li>
+						<a href="https://www.youtube.com/embed/JsZoKrxJbjs"><img src="images/sandwich.jpg" alt="Image"></a>
+						<div>
+							<h2><a href="https://www.youtube.com/embed/JsZoKrxJbjs">Ham Sandwich</a></h2>
+
+							
+							<span>by: Anna</span>
+						</div>
+					</li>
+					
+					<li>
+						<a href="https://www.youtube.com/embed/sv3TXMSv6Lw""><img src="images/pizza.jpg" alt="Image"></a>
+						<div>
+
+					
+							<h2><a href="recipes.html">Delicious Pizza</a></h2>
+							<span>by: Rico</span>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<h3>Blog</h3>
+				<ul id="blog">
+					
+					
+				
+			</div>
+			
+		</div>
+	</div>
+	<div class="footer">
+		<div>
+			<p>
+				&copy;Copyright 2023. All rights reserved
+			</p>
+		</div>
+	</div>
+</body>
+</html>
